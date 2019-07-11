b33993c5ffc2fa8ab6db512c83706f3e8ff681fe
Данил Шипилов <danil.shipilov.99@gmail.com>
2019-07-11 15:02:25 +0600

    DpHw-4

---
+++ DpHw-4.Mediator/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpHw_4.Mediator
+{
+    // Класс менеджера в методе Send() проверяет, от кого пришло сообщение, 
+    // и в зависимости от отправителя перенаправляет его другому объекту с 
+    // помощью методов Notify(), определенных в классе Colleague.
+    class ManagerMediator : Mediator
+    {
+        public Colleague Customer { get; set; }
+        public Colleague Cook { get; set; }
+        public Colleague Tester { get; set; }
+        public override void Send(string msg, Colleague colleague)
+        {
+            // если отправитель - заказчик, значит есть новый заказ
+            // отправляем сообщение повару - выполнить заказ
+            if (Customer == colleague)
+                Cook.Notify(msg);
+            // если отправитель - повар, то можно приступать к дегустации
+            // отправляем сообщение дегустатору
+            else if (Cook == colleague)
+                Tester.Notify(msg);
+            // если отправитель - дегустатор, значит блюдо готов
+            // отправляем сообщение заказчику
+            else if (Tester == colleague)
+                Customer.Notify(msg);
+        }
+    }
+
+    abstract class Mediator
+    {
+        public abstract void Send(string msg, Colleague colleague);
+    }
+    abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public virtual void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+        public abstract void Notify(string message);
+    }
+    // Класс заказчика
+    class CustomerColleague : Colleague
+    {
+        public CustomerColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение заказчику: " + message);
+        }
+    }
+    // Класс повара
+    class CookColleague : Colleague
+    {
+        public CookColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение повару: " + message);
+        }
+    }
+    // Класс дегустатора
+    class TesterColleague : Colleague
+    {
+        public TesterColleague(Mediator mediator)
+            : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение дегустатору: " + message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagerMediator mediator = new ManagerMediator();
+            Colleague customer = new CustomerColleague(mediator);
+            Colleague cook = new CookColleague(mediator);
+            Colleague tester = new TesterColleague(mediator);
+            mediator.Customer = customer;
+            mediator.Cook = cook;
+            mediator.Tester = tester;
+            customer.Send("Есть заказ, надо приготовить блюдо");
+            cook.Send("Блюдо готово, надо продегустировать");
+            tester.Send("Блюдо продегустировано и готово к подаче на стол");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DpHw-4.Memento/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DpHw_4.Memento
+{
+    // Originator
+    // Класс героя имеет состояние, которое может со временем
+    // меняться. Он также объявляет методы SaveState() и RestoreState().
+    // С их помощью объект Hero может сохранить свое состояние 
+    // в HeroMemento, а в будущем его восстановить.
+    class Hero
+    {
+        private int potions = 10; // кол-во зелий
+        private int healthPoints = 5; // кол-во HP
+
+        public void Drink()
+        {
+            if (potions > 0)
+            {
+                potions--;
+                healthPoints += 10;
+                Console.WriteLine("Пью зелье лечения.");
+                Console.WriteLine("НР - {0}", healthPoints);
+                Console.WriteLine("Зелий - {0}\n", potions);
+            }
+            else
+                Console.WriteLine("Зелий больше нет");
+        }
+        // сохранение состояния
+        public HeroMemento SaveState()
+        {
+            Console.WriteLine("Сохранение игры. Параметры: {0} зелий, {1} НР\n", potions, healthPoints);
+            return new HeroMemento(potions, healthPoints);
+        }
+
+        // восстановление состояния
+        public void RestoreState(HeroMemento memento)
+        {
+            this.potions = memento.Patrons;
+            this.healthPoints = memento.Lives;
+            Console.WriteLine("Восстановление игры. Параметры: {0} зелий, {1} НР\n", potions, healthPoints);
+        }
+    }
+
+    class HeroMemento
+    {
+        public int Patrons { get; private set; }
+        public int Lives { get; private set; }
+
+        public HeroMemento(int patrons, int lives)
+        {
+            this.Patrons = patrons;
+            this.Lives = lives;
+        }
+    }
+
+    // Класс GameHistory предназначен для хранения состояний, 
+    // причем все состояния хранятся в стеке, что позволяет 
+    // с легкостью извлекать последнее сохраненное состояние.
+    class GameHistory
+    {
+        public Stack<HeroMemento> History { get; private set; }
+        public GameHistory()
+        {
+            History = new Stack<HeroMemento>();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero();
+            hero.Drink(); // пьем зелье, осталось 9
+            GameHistory game = new GameHistory();
+
+            game.History.Push(hero.SaveState()); // сохраняем игру
+
+            hero.Drink(); //пьем зелье, осталось 8
+
+            hero.RestoreState(game.History.Pop()); // востанавливаем игру
+
+            hero.Drink(); //пьем зелье, осталось 8
+
+            Console.ReadLine();
+        }
+    }
+}
